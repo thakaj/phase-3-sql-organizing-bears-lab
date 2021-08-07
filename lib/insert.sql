b42e6a800b62b937d87453ad96ed29233d04333c
thakaj <taulant.hakaj@scranton.edu>
2021-08-07 11:20:46 -0400

    commit tree 5ab5fbc21c123635703c0cd7e43b4a05893b1bf0

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (1,"steve",3,"M", "brown","nice", 1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (2,"f",4,"M","brown","nice", 1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (3,"g",4,"M","brown","nice", 1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (4,"h",4,"M","brown","nice", 1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (5,"j",4,"F","brown","nice", 1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (6,"k",4,"M","brown","nice", 0);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (7,"l",4,"M","brown","nice", 0);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (8, null ,4,"F","brown","goofy", 0);
